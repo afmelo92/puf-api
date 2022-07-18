74606f37dc8bbd5091cc333adee7faa30cbc77a2
Andre Melo <andre.fabian.melo@gmail.com>
2022-07-18 15:53:35 -0300

    refactor: update logger style according to envinronment

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
-import winston from 'winston';
-
-import { logger } from './utils/logger';
 
 const app = new Koa();
 
 import { router } from './modules/routes';
-
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.simple(),
-                winston.format.errors({ stack: true }),
-                winston.format.timestamp(),
-                winston.format.prettyPrint()
-            ),
-        })
-    );
-}
 
 app.use(bodyParser());
 app.use(router.routes());
